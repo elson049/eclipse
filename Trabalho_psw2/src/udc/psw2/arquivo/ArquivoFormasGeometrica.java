0e78d5e97ed3e3a15f3c2a7fa6773cb3a864024e
elson <elson@vivobook.local>
2019-04-05 12:18:59 -0300

    Interface arquivos

--- Trabalho_psw2/src/udc/psw2/arquivo/ArquivoFormasGeometrica.java
+++ Trabalho_psw2/src/udc/psw2/arquivo/ArquivoFormasGeometrica.java
@@ -4,8 +4,6 @@ import udc.psw2.FigurasGeometricas.FiguraGeometrica;
 import udc.psw2.lista.ListaEncadeada;
 
 public interface ArquivoFormasGeometrica {
-	// Metodo AlgorithmInterface() da classe Strategy do padr�o Strategy
 	public ListaEncadeada<FiguraGeometrica> lerFormas();
-	// Metodo AlgorithmInterface() da classe Strategy do padr�o Strategy
 	public void salvarFormas(ListaEncadeada<FiguraGeometrica> lista);
 }
