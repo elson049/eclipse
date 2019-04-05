391460549e866dbc4d45bc96385b30470392cc43
elson <elson@vivobook.local>
2019-04-05 12:09:40 -0300

    Atualização classe app

--- Trabalho_psw2/src/udc/psw2/aplicacao/App.java
+++ Trabalho_psw2/src/udc/psw2/aplicacao/App.java
@@ -4,62 +4,32 @@ import java.awt.Point;
 
 import javax.swing.JFrame;
 
-import udc.psw2.FigurasGeometricas.FiguraGeometrica;
 import udc.psw2.gui.FramePrincipal;
-import udc.psw2.lista.Iterador;
-import udc.psw2.lista.ListaEncadeada;
-import udc.psw2.lista.OuvinteLista;
-
 
 public class App {
 	
-	private ListaEncadeada<FiguraGeometrica> listaFiguras;
-	private ListaEncadeada<OuvinteLista> listaOuvinte;
 	private static App app;
+	private Documento doc;
 
 	private App() {
-		listaFiguras= new ListaEncadeada<FiguraGeometrica>();
-		listaOuvinte=new ListaEncadeada<OuvinteLista>();
-
+		doc = new Documento();
+		FramePrincipal frame = new FramePrincipal(doc);
+		frame.setTitle("Painel de Desenho");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocation(new Point(300, 300));
+		frame.setSize(800, 600); // configura o tamanho do frame
+		frame.setVisible(true);
+	}
+	public Documento getDocumentAtivo() {
+		return doc;
 	}
 	public static App getApp() {
 		if(app == null)
 			app= new App();
 		return app;
 	}
-	public void inserirOuvinte(OuvinteLista ouvinte) {
-		listaOuvinte.inserir(ouvinte,0);
-	}
-	public void inserirFormaGeometrica(FiguraGeometrica forma) {
-		listaFiguras.inserir(forma,0);
-		Iterador<OuvinteLista> i =listaOuvinte.getInicio();
-		OuvinteLista ouvinte;
-		while((ouvinte = (OuvinteLista)i.proximo()) != null) {
-			ouvinte.atualizarFigura();
-		}
-	}
-	public Iterador<FiguraGeometrica> getIndice() {
-		return listaFiguras.getInicio();
-	}
-	public boolean Lista_vazia () {
-		return listaFiguras.isEmpty();
-	}
-	public void EscruirInicio() {
-		listaFiguras.excluirInicio();
-		return;
-	}
-	public void LimparPainel() {
-		listaFiguras.limpar();
-		listaOuvinte.limpar();
-	}
 	public static void main(String[] args) {
-		
-		FramePrincipal frame = new FramePrincipal();
-		frame.setTitle("Painel de Desenho");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLocation(new Point(300, 300));
-		frame.setSize(800, 600); // configura o tamanho do frame
-		frame.setVisible(true);
+		App.getApp();
 	}
 
 }
