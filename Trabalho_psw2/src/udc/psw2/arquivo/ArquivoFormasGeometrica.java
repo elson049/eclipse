013e84d5da45fa634c8f92104adb4a3fc12999b9
elson <elson@vivobook.local>
2019-04-07 17:26:09 -0300

    Identação

--- Trabalho_psw2/src/udc/psw2/arquivo/ArquivoFormasGeometrica.java
+++ Trabalho_psw2/src/udc/psw2/arquivo/ArquivoFormasGeometrica.java
@@ -1,9 +1,10 @@
 package udc.psw2.arquivo;
 
+
 import udc.psw2.FigurasGeometricas.FiguraGeometrica;
 import udc.psw2.lista.ListaEncadeada;
 
-public interface ArquivoFormasGeometrica {
+public interface ArquivoFormasGeometrica{
 	public ListaEncadeada<FiguraGeometrica> lerFormas();
 	public void salvarFormas(ListaEncadeada<FiguraGeometrica> lista);
 }
